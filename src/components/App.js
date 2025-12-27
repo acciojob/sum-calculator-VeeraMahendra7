1a0b4432ef2c6bb3c5019208368b6ee27d110053
VeeraMahendra7 <veeramahindra787@gmail.com>
2025-12-27 20:30:38 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,42 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [nums, setNums] = useState([]);
+  const [curr, setCurr] = useState();
+  const [sum, setSum] = useState(0);
+
+  useEffect(()=>{
+
+    const timer = setTimeout(()=>{
+      const total = nums.reduce((acc,curr) => acc+curr, 0);
+      setSum(total);
+    },0);
+
+    return () => clearTimeout(timer);
+    
+  },[nums]);
+
+
+  const handleChange = (e) => {
+    const value = parseInt(e.target.value);
+    setCurr(e.target.value);
+
+    if(!isNaN(value)){
+        setNums(prev => [...prev, value]);
+    }
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <h1>Sum Calculator</h1>
+        <input type="number" value={curr} onChange={handleChange} placeholder="Enter a Number"/>
+        {
+          nums.length > 0 && (
+            <p>Sum: {sum}</p>
+          )
+        }
     </div>
   )
 }
